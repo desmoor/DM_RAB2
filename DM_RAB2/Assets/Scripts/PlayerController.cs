5ad85c831d4f78351b1a0d087757f712ece3f9ce
ADS\desmoor <desmoor@iu.edu>
2019-02-18 18:02:39 -0500

    Added jump ability through changes to the Player script.

--- DM_RAB2/Assets/Scripts/PlayerController.cs
+++ DM_RAB2/Assets/Scripts/PlayerController.cs
@@ -8,13 +8,23 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Text countText;
     public Text winText;
+    public LayerMask groundLayers;
+    public float jumpForce = 7;
+    public SphereCollider col;
 
     private Rigidbody rb;
     private int count;
 
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
+            col.bounds.min.y, col.bounds.center.z), col.radius * 0.9f, groundLayers);
+    }
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
         count = 0;
         SetCountText();
         winText.text = "";
@@ -28,6 +38,11 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
     }
 
     void OnTriggerEnter(Collider other)
